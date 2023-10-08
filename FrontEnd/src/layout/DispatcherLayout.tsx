1b695b8f55ff316ff0ecdb48d8642c90c48c8783
Frayaa <fitrah.fraya@gmail.com>
2023-10-08 07:28:02 +0700

    logout

--- FrontEnd/src/layout/DispatcherLayout.tsx
+++ FrontEnd/src/layout/DispatcherLayout.tsx
@@ -1,47 +1,53 @@
-import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
-import { ReactNode } from "react";
+import { Box, Button, Flex, Image, Text } from "@chakra-ui/react"
+import { ReactNode } from "react"
 import smartParkLogo from "../assets/icons/full-logo.svg"
-import { DispatcherNavigation } from "../components/DispatcherNavigation";
+import { DispatcherNavigation } from "../components/DispatcherNavigation"
+import { useNavigate } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import { LOGOUT } from "../store/rootReducer"
 
 interface LayoutProps {
   children: ReactNode
 }
 
 export function DispatcherLayout({ children }: LayoutProps) {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const btnLogout = () => {
+    localStorage.removeItem("token")
+    dispatch(LOGOUT())
+    navigate("/auth/login")
+  }
   return (
-    <Flex justify={'center'} minH={'100vh'}>
+    <Flex justify={"center"} minH={"100vh"}>
       <Box
-        w={'30%'}
+        w={"30%"}
         position={"sticky"}
         top={0}
         height={"100vh"}
         borderRight={"1px"}
-        borderColor={'gray.200'}
+        borderColor={"gray.200"}
       >
-        <Box position={'relative'} h={'100vh'}>
+        <Box position={"relative"} h={"100vh"}>
           <Flex
-            direction={'column'}
-            justify={'center'}
-            align={'center'}
+            direction={"column"}
+            justify={"center"}
+            align={"center"}
             pt={10}
             pb={5}
           >
             <Image w={100} src={smartParkLogo} />
-            <Text fontSize={'smaller'}>For Officers</Text>
+            <Text fontSize={"smaller"}>For Officers</Text>
           </Flex>
 
           <DispatcherNavigation />
 
-          <Box
-            position={'absolute'}
-            bottom={0}
-            w={'full'}
-            p={5}
-          >
+          <Box position={"absolute"} bottom={0} w={"full"} p={5}>
             <Button
-              w={'full'}
-              justifyContent={'left'}
-              variant={'ghost'}
+              w={"full"}
+              justifyContent={"left"}
+              variant={"ghost"}
+              onClick={btnLogout}
             >
               Logout
             </Button>
@@ -49,10 +55,7 @@ export function DispatcherLayout({ children }: LayoutProps) {
         </Box>
       </Box>
 
-      <Box
-        w={'70%'}
-        bgColor={'whitesmoke'}
-      >
+      <Box w={"70%"} bgColor={"whitesmoke"}>
         {children}
       </Box>
     </Flex>
